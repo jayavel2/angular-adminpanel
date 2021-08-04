1de55593e2886fe6e8e42c98e135ca96431bdaac
jayavel2 <82749755+jayavel2@users.noreply.github.com>
2021-08-04 15:43:04 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   {
     path      :'',
-    redirectTo : 'dashboard',
+    redirectTo : 'login',
     pathMatch : 'full'
   },
   {
